15f03723ddfea360fee91a821b1b12c45f0bdb6d
OctoSheep_Manjaro <zyymaker@outlook.com>
2023-04-19 15:08:23 +0800

    我的页面可修改用户昵称

--- src/pages/my/my.jsx
+++ src/pages/my/my.jsx
@@ -13,16 +13,18 @@
 
 import './my.less';
 
-import {Component}  from 'react';
-import {Text, View} from '@tarojs/components';
-import Taro         from '@tarojs/taro';
-import {Input}      from '@nutui/nutui-react-taro';
+import {Component}           from 'react';
+import {Text, View}          from '@tarojs/components';
+import Taro                  from '@tarojs/taro';
+import {Button, Icon, Input} from '@nutui/nutui-react-taro';
+import {updateUser}          from '@/api/api';
 
 class My extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userData: {},
+      newName:  '',
       loading:  true,
     };
   }
@@ -40,6 +42,33 @@ class My extends Component {
     });
   }
 
+  updateUser = (name) => {
+    const {userData} = this.state;
+    userData.name    = name;
+
+    updateUser(
+      userData.openid,
+      userData.name,
+      userData.globalData,
+      userData.glossaries,
+      userData.todayCount,
+    ).then(() => {
+      this.setState({
+        userData: userData,
+        loading:  false,
+      });
+
+      Taro.setStorage({
+        key:  'userData',
+        data: userData,
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  };
+
   render() {
     const {
             userData,
@@ -54,8 +83,39 @@ class My extends Component {
             label={'昵称'}
             placeholder={'请输入昵称'}
             defaultValue={userData.name}
+            required={true}
+            onBlur={(value) => {
+              this.setState({
+                newName: value,
+              });
+            }}
+            slotButton={
+              <Button
+                type={'primary'}
+                size={'small'}
+                onClick={() => {
+                  if (this.state.newName !== '') {
+                    this.setState({
+                      loading: true,
+                    });
+                    this.updateUser(this.state.newName);
+                  } else {
+                    Taro.showToast({
+                      title: '昵称不能为空',
+                      icon:  'error',
+                      mask:  true,
+                    }).catch((err) => {
+                      console.log(err);
+                    });
+                  }
+                }}
+              ><Icon
+                name={'checklist'}
+                size={'1em'}
+              />
+              </Button>
+            }
           />
-          <Text>成功</Text>
         </View>
       );
     } else {
